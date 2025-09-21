cbdbd44c0bbc6ecd87edce8a5cd4b1e8820e497e
gazarabbanii1 <gaza.rabbanii@gmail.com>
2025-09-21 11:05:44 +0800

    Add files via upload

---
+++ node.java
@@ -0,0 +1,13 @@
+package SingleLinkedlist;
+
+class Node{
+    String name;
+    int NoAntrian;
+    Node next;
+
+    public Node (String name, int NoAntrian){
+        this.name = name;
+        this.NoAntrian = NoAntrian;
+        this.next = null;
+    }
+}
